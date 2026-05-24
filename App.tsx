3a9943d600e43aa0d3495e829ab130475bee8315
Yiquan <v1oyiquanwei@gmail.com>
2026-05-24 14:18:41 -0700

    Fix GestureHandlerRootView for web

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import WriteScreen from './src/screens/WriteScreen';
@@ -10,19 +12,25 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Write">
-        <Stack.Screen
-          name="Write"
-          component={WriteScreen}
-          options={{ title: '占卜' }}
-        />
-        <Stack.Screen
-          name="Result"
-          component={ResultScreen}
-          options={{ title: '卦象' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={styles.root}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Write">
+          <Stack.Screen
+            name="Write"
+            component={WriteScreen}
+            options={{ title: '占卜' }}
+          />
+          <Stack.Screen
+            name="Result"
+            component={ResultScreen}
+            options={{ title: '卦象' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  root: { flex: 1 },
+});
